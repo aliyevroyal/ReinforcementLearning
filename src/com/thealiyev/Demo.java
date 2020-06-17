05dd5bb7d4e3e8030d86a3c73969647a52d19bd3
Royal Aliyev <royalaliyev@Royals-MacBook-Air.local>
2020-06-17 10:46:48 +0300

    Initial commit

--- src/com/thealiyev/Demo.java
+++ src/com/thealiyev/Demo.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 
 public class Demo {
     public static void main(String[] args) {
-        //Rewards table initialization
+        /**
+         * Example shows how to initialize Rewards table
+         */
         ArrayList<ArrayList<Double>> QTable, RTable = new ArrayList<>();
         ArrayList<Double> vector = new ArrayList<>();
         for (int stCounter = 0; stCounter < 4; stCounter++) {
             for (int ndCounter = 0; ndCounter < 4; ndCounter++) {
-                if (stCounter == ndCounter) {
+                if (stCounter == ndCounter || ndCounter < stCounter) {
                     vector.add(-1.0);
                 } else {
                     vector.add(0.0);
@@ -21,8 +23,6 @@ public class Demo {
         RTable.get(0).set(1, 70.0);
         RTable.get(0).set(3, -1.0);
         RTable.get(1).set(3, -1.0);
-        RTable.get(3).set(0, -1.0);
-        RTable.get(3).set(1, -1.0);
         RTable.get(2).set(3, 100.0);
         System.out.println("Reward table:");
         for (int stCounter = 0; stCounter < RTable.size(); stCounter++) {
@@ -32,15 +32,20 @@ public class Demo {
             System.out.println();
         }
 
+        /**
+         * Example shows how to Q-value learning library is being called and all essential arguments are being set
+         */
         QValueLearning QValueLearning = new QValueLearning();
-        QValueLearning.setInitialState(0);
         QValueLearning.setFinalState(3);
         QValueLearning.setAlpha(0.7);
         QValueLearning.setOmega(0.8);
         QValueLearning.setRTable(RTable);
-        QValueLearning.iterate();
+        QValueLearning.iterate(130);
 
-        System.out.println("\nQ-values table after iterations:");
+        /**
+         * Example shows how to get Q-value table at the end of iterations calling getQTable() method so you can integrate it in your application
+         */
+        System.out.println("Q-values table after iterations:");
         QTable = QValueLearning.getQTable();
         for (int stCounter = 0; stCounter < QTable.size(); stCounter++) {
             for (int ndCounter = 0; ndCounter < QTable.get(stCounter).size(); ndCounter++) {
--- src/com/thealiyev/QValueLearning.java
+++ src/com/thealiyev/QValueLearning.java
@@ -9,40 +9,74 @@ public class QValueLearning {
     private double alpha, omega;
     private int theNumberOfActions, theNumberOfStates;
     private ArrayList<ArrayList<Double>> RTable, QTable;
-    private double QValue, MaxQValue, reward;
-    ArrayList<Integer> possibleActions = new ArrayList<>();
-    private int initialState, finalState, currentState, action;
+    private int initialState, finalState;
 
-    public void iterate() {
+    /**
+     * Iterations
+     */
+    public void iterate(int iterations) {
         random = new Random();
-        //Initialization starts here
+        for (int counter = 0; counter < iterations; counter = counter + 1) {
+            System.out.println("Iteration number: " + counter);
+            if (counter == 0) {
+                createEmptyQTable();
+            }
+            initialState = random.nextInt(theNumberOfStates);
+            episode();
+        }
+    }
+
+    /**
+     * An episode
+     */
+    public ArrayList<ArrayList<Double>> episode() {
+        random = new Random();
+        double QValue, MaxQValue, reward;
+        ArrayList<Integer> possibleActions;
+        int currentState, action;
+        //Initialization starts here...
         currentState = initialState;
-        //Iterations start here
+        //An episode start from here...
         while (currentState != finalState) {
-            System.out.println("Current State: " + currentState);
-            //Find out all possible actions for current state
+            //Finds out all the possible actions for current state
             possibleActions = new ArrayList<>();
             for (int stCounter = 0; stCounter < RTable.get(currentState).size(); stCounter++) {
                 if (RTable.get(currentState).get(stCounter) >= 0) {
                     possibleActions.add(stCounter);
                 }
             }
-            System.out.println("All Possible Actions: " + possibleActions);
-
-            //Find action for current state with the highest Q Value
-            MaxQValue = QTable.get(currentState).get(possibleActions.get(0));
+            //Finds out an action for current state with the highest Q Value
             action = possibleActions.get(0);
+            QValue = QTable.get(currentState).get(action);
             reward = RTable.get(currentState).get(action);
             for (int stCounter = 0; stCounter < possibleActions.size(); stCounter++) {
-                if (QTable.get(currentState).get(possibleActions.get(stCounter)) > MaxQValue) {
-                    MaxQValue = QTable.get(currentState).get(possibleActions.get(stCounter));
+                if (QTable.get(currentState).get(possibleActions.get(stCounter)) > QValue) {
                     action = possibleActions.get(stCounter);
+                    QValue = QTable.get(currentState).get(action);
                     reward = RTable.get(currentState).get(action);
+                } else if (QTable.get(currentState).get(possibleActions.get(stCounter)) == QValue) {
+                    if (random.nextBoolean()) {
+                        action = possibleActions.get(stCounter);
+                        QValue = QTable.get(currentState).get(action);
+                        reward = RTable.get(currentState).get(action);
+                    }
+                }
+            }
+            //Finds out all the possible actions for next state
+            possibleActions = new ArrayList<>();
+            for (int stCounter = 0; stCounter < RTable.get(action).size(); stCounter++) {
+                if (RTable.get(currentState).get(stCounter) >= 0) {
+                    possibleActions.add(stCounter);
+                }
+            }
+            //Finds Q max
+            MaxQValue = QTable.get(action).get(possibleActions.get(0));
+            for (int stCounter = 0; stCounter < possibleActions.size(); stCounter++) {
+                if (QTable.get(currentState).get(possibleActions.get(stCounter)) > MaxQValue) {
+                    MaxQValue = QTable.get(action).get(possibleActions.get(stCounter));
                 } else if (QTable.get(currentState).get(possibleActions.get(stCounter)) == MaxQValue) {
                     if (random.nextBoolean()) {
-                        MaxQValue = QTable.get(currentState).get(possibleActions.get(stCounter));
-                        action = possibleActions.get(stCounter);
-                        reward = RTable.get(currentState).get(action);
+                        MaxQValue = QTable.get(action).get(possibleActions.get(stCounter));
                     }
                 }
             }
@@ -53,13 +87,13 @@ public class QValueLearning {
             //Update state
             currentState = action;
         }
-        System.out.println("Current state: " + finalState);
+        return QTable;
     }
 
     /**
      * Creates empty Q values table each element equals to 0.0
      */
-    private void createEmptyQTable() {
+    public void createEmptyQTable() {
         QTable = new ArrayList<>();
         ArrayList<Double> vector = new ArrayList<>();
 
@@ -81,7 +115,6 @@ public class QValueLearning {
         this.RTable = RTable;
         this.theNumberOfStates = RTable.size();
         this.theNumberOfActions = RTable.get(0).size();
-        createEmptyQTable();
     }
 
     /**
@@ -96,13 +129,6 @@ public class QValueLearning {
      */
     public void setOmega(double omega) {
         this.omega = omega;
-    }
-
-    /**
-     * Sets initial state
-     */
-    public void setInitialState(int initialState) {
-        this.initialState = initialState;
     }
 
     /**
